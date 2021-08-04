89f2132d3b2594cec56b2371c6bfe78414e908c1
Artem <arthyrych@gmail.com>
2021-08-04 08:00:29 +0300

    create-todo test added, open-todos.spec.js refactored

--- cypress/integration/ui/01-open-todos.spec.js
+++ cypress/integration/ui/01-open-todos.spec.js
@@ -1,33 +1,35 @@
 describe('open todos test suite', () => {
 
-    it('open mixed todos from fixtures', () => {
-        cy.intercept('GET', '/todos', {fixture: 'mixed-todos-list'}).as('getTodos')
+    function interceptTodos (fixture) {
+        cy.intercept('GET', '/todos', fixture).as('getTodos')
         cy.visit('/')
         cy.wait('@getTodos')
-        cy.get('#todo-list').should('be.visible')
-            .find('.todo').should('have.length', 4).and('not.be.empty')
+    }
+
+    it('open completed/uncompleted todos from fixtures', () => {
+        interceptTodos({fixture: 'mixed-todos-list'})
+        cy.get('#todo-list').should('be.visible').find('.todo').then(todos => {
+            cy.wrap(todos).should('have.length', 4).and('not.be.empty')
+            cy.wrap(todos).first().should('not.have.class', 'completed')
+            cy.wrap(todos).last().should('have.class', 'completed')
+        })   
+        
     })
 
-    it('open none todos from fixtures', () => {
-        cy.intercept('GET', '/todos', {fixture: 'empty-todos-list'}).as('getTodos')
-        cy.visit('/')
-        cy.wait('@getTodos')
+    it('open empty todos list from fixtures', () => {
+        interceptTodos({fixture: 'empty-todos-list'})
         cy.get('#todo-list').should('not.be.visible')
             .find('.todo').should('not.exist')
     })
 
     it('open completed todos from fixtures', () => {
-        cy.intercept('GET', '/todos', {fixture: 'completed-todos-list'}).as('getTodos')
-        cy.visit('/')
-        cy.wait('@getTodos')
+        interceptTodos({fixture: 'completed-todos-list'})
         cy.get('#todo-list').should('be.visible')
             .find('.todo').should('have.length', 4).and('have.class', 'completed')
     })
 
     it('open uncompleted todos from fixtures', () => {
-        cy.intercept('GET', '/todos', {fixture: 'uncompleted-todos-list'}).as('getTodos')
-        cy.visit('/')
-        cy.wait('@getTodos')
+        interceptTodos({fixture: 'uncompleted-todos-list'})
         cy.get('#todo-list').should('be.visible')
             .find('.todo').should('have.length', 4).and('not.have.class', 'completed')
     })
--- cypress/integration/ui/02-create-todo.spec.js
+++ cypress/integration/ui/02-create-todo.spec.js
@@ -1,13 +1,50 @@
 describe('create todo test suite', () => {
 
+    const unique = Date.now()
+
+    const newTodos = [
+        "first unique todo via ui " + unique,
+        "second unique todo via ui " + unique,
+        "third unique todo via ui " + unique,
+        "fourth unique todo via ui " + unique,
+    ]
+
+    function todoCreateAndValidate (todoNumber) {
+        cy.get('#add-todo').should('be.visible').and('have.class', 'new-todo')
+            .type(newTodos[todoNumber] + "{enter}")
+        cy.contains(newTodos[todoNumber]).should('be.visible') 
+    }
+
     before(() => {
-        cy.task('resetDatabase')
         cy.visit('/')
     })
 
-    it('valid todo creation', () => {
+    it('precreation check', () => {
         cy.get('.todo').should('not.exist')
     })
 
+    it('first todo creation', () => {
+        todoCreateAndValidate(0)
+    })
+
+    it('second todo creation', () => {
+        todoCreateAndValidate(1)
+    })
+
+    it('third todo creation', () => {
+        todoCreateAndValidate(2)
+    })
+
+    it('fourth todo creation', () => {
+        todoCreateAndValidate(3)
+    })
+
+    it('length check', () => {
+        cy.get('.todo').should('have.length', 4)
+    })
+
+    after(() => {
+        cy.task('resetDatabase')
+    })
 
 })
